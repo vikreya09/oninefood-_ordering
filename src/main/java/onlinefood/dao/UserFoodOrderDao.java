9fc8d3f87e61ff7c6246658e8e3b6f0b0e2250a0
vikreya09 <106046434+vikreya09@users.noreply.github.com>
2022-05-30 16:22:14 +0530

    initial commit

--- src/main/java/onlinefood/dao/UserFoodOrderDao.java
+++
@@ -1,9 +0,0 @@
-package onlinefood.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import onlinefood.model.UserFoodOrder;
-
-public interface UserFoodOrderDao extends JpaRepository<UserFoodOrder, Integer> {
-
-}
